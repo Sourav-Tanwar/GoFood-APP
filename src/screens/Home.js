4381440001d053094d7526316875d6abc9c753f2
Sourav-Tanwar <souravtnw@gmail.com>
2023-05-23 16:22:17 +0530

    Added Navbar, Footer, Card and Carousel

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-
+import React from "react";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <div>
-        <div><Navbar/></div>
-        <div>Body</div>
-        <div><Footer/></div>
-
+      <div> <Navbar /></div>
+      <div> <Carousel /></div>
+      <div className="m-3"> 
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      </div>
+      
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
