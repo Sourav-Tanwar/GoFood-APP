57c53e1eaa3a3b016fa867cf6f4b466a823c2e2f
Sourav-Tanwar <souravtnw@gmail.com>
2023-06-17 16:11:53 +0530

    fetching data from BE, Card update

--- backend/db.js
+++ backend/db.js
@@ -13,20 +13,27 @@ const mongoDB =async () =>{
         const  fetched_data = await mongoose.connection.db.collection("food_items");
         
         // fetched_data.findOne().then(function(data){
+            const food_data_array =[];
             fetched_data.find({}).forEach((data)=> {
-            // console.log(data);
+                food_data_array.push(data)
+                // console.log(data);
+                // global.food_items = data;
+                // console.log(global.food_items)
 
         })
+        global.food_items = food_data_array;
     }
 
     const foodCategory =async()=>{
         const  fetched_data = await mongoose.connection.db.collection("foodCategory");
         
+        const Category_data_array =[];
         // fetched_data.findOne().then(function(data){
             fetched_data.find({}).forEach((data)=> {
                 // console.log(data);
-            
+                Category_data_array.push(data)
         })
+        global.food_category = Category_data_array;
     }
     
     foodItem();
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
+
+  let options = props.options;
+  let priceOptions = Object.keys(options)
+
   return (
     <div>
         <div class="card mt-3" style={{"width": "18rem", "maxHeight":"360px"}}>
-          <img src="https://source.unsplash.com/random/900x600?burger" class="card-img-top" alt="..." />
+          <img src={props.imgSrc} class="card-img-top" alt="..." />
           <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is important text.</p>
+          <h5 class="card-title">{props.foodName}</h5>
             <div className="container w-100">
               <select className="m-2 h-100 bg-success rounded">
                 {Array.from(Array(6),(e,i)=>{
@@ -17,8 +20,11 @@ export default function Card() {
                 })}
               </select>
               <select className="m-2 h-100 bg-success rounded">
-                <option value="half">Half</option>
-                <option value="Full">Full</option>
+                  {priceOptions.map((data)=>{
+                    return <option key={data} value={data}>{data}</option>
+                  })
+
+                  }
               </select>
               <div className="d-inline h-100 fs-5">
                 Total Price
