1e7912e8c74ed74a3b293788a4f6650f8b9383db
Sourav-Tanwar <souravtnw@gmail.com>
2023-06-20 13:28:44 +0530

    Add, Remove, Update functionality on CART

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useState } from 'react'
 import {Link,useNavigate} from "react-router-dom";
+import Badge from 'react-bootstrap/Badge';
+import Cart from "../screens/Cart";
+import Modal from "../Modal";
+import { useCart } from './ContextReducer';
+
 
 function Navbar() {
 
-const navigate = useNavigate();
-const handleLogout =()=>{
-  localStorage.removeItem("authToken");
-  navigate("/login")
-}
+  let data =useCart();
+  const [cartView,setCartView] = useState(false);
+  const navigate = useNavigate();
+  const handleLogout =()=>{
+    localStorage.removeItem("authToken");
+    navigate("/login")
+  }
 
   return (
     <div>
+      
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
           <div className="container-fluid">
             <Link className="navbar-brand fs-4 fst-italic"  to="/">GoFood</Link>
@@ -37,7 +45,11 @@ const handleLogout =()=>{
                 </div>
                 :
                 <div>
-                    <div className='btn bg-white text-success mx-2'>My Cart</div>
+                    <div className='btn bg-white text-success mx-2' onClick={()=>setCartView(true)}>
+                      My Cart {" "}
+                      <Badge pill bg="danger" >{data.length}</Badge>
+                    </div>
+                    {cartView ? <Modal onClose={()=> setCartView(false)}><Cart/></Modal>:null}
                     <div className='btn bg-white text-danger mx-2' onClick={handleLogout}>Logout</div>
                 </div>
               }              
