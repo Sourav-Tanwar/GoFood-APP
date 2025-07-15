f40e96e906545f7d1048234d388cc4e22f421788
Sourav-Tanwar <souravtnw@gmail.com>
2025-07-15 11:05:34 +0530

    CORS updated

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,14 @@ const cors = require("cors");
 
 mongoDB();
 
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:5000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Orgin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin","http://localhost:5000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Orgin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
 
 app.use(cors());
 app.use(express.json())
